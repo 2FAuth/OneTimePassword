abf1952daafe2da2d2a74bdaf12a9fba20beb415
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-03-01 14:39:38 +0300

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "OneTimePassword",
+    platforms: [
+        .iOS(.v8),
+        .watchOS(.v2),
+    ],
+    products: [
+        .library(
+            name: "OneTimePassword",
+            targets: ["OneTimePassword"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/podkovyrin/Base32",
+            .revision("2901af815f77d869af0631a319e2ed7efde79cee")
+        )
+    ],
+    targets: [
+        .target(
+            name: "OneTimePassword",
+            dependencies: ["Base32"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "OneTimePasswordTests",
+            dependencies: ["OneTimePassword"],
+            path: "Tests",
+            exclude: ["KeychainTests.swift", "App"]
+        )
+    ]
+)
